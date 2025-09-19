68a494630b7db46b1750078e9d53ad5a4a80f8b5
santiagorq2405 <santiagorq2405@gmail.com>
2025-09-19 17:24:42 +0200

    Version Inicial

---
+++ jsSample.js
@@ -0,0 +1,8 @@
+console.log(" Hello, World! ");
+console.log(" Hola Mundo! ");
+console.log(" Bonjour le monde! ");
+console.log(" Hallo Welt! ");      // Alemán
+console.log(" Ciao mondo! ");     // Italiano
+console.log(" Olá Mundo! ");      // Portugués
+console.log(" Привет, мир! ");    // Ruso
+console.log(" こんにちは世界! ");   // Japonés
